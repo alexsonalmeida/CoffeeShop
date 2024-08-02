d0a6c4c690221198076abd0543dc51025db77ebd
Alexson Almeida <as3904527@gmail.com>
2024-08-02 10:06:41 -0300

    Initial Commit

---
+++ Exemple/ExempleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExempleApp.swift
+//  Exemple
+//
+//  Created by Francisco Alexson Almeida da Silva on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ExempleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
